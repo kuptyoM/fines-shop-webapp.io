2bb968d7acffaecf1673d99e1a37c59de21a804c
kuptyoM <zxcsfzoom@mail.ru>
2023-11-15 20:43:40 +0300

    Deployed website

--- my-app/src/components/screens/Account/Account.js
+++ my-app/src/components/screens/Account/Account.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import styles from './Account.module.css'
-
+import { Link } from 'react-router-dom';
 
 function Account() {
     return (
-        <div className={styles.container}>
-            Личный кабинет
-        </div>
+        <Link to="/fines-shop-webapp.io/accountdetail" className={styles.container}>
+                <div>
+                    Личный кабинет
+                </div>
+            </Link>
     )
 }
 
--- my-app/src/components/screens/Avaibility/Avaibility.js
+++ my-app/src/components/screens/Avaibility/Avaibility.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import styles from './Avaibility.module.css'
-
+import { Link } from 'react-router-dom';
 
 function Avaibility() {
     return (
-        <div className={styles.container}>
-            Наличие
-        </div>
+        <Link to="/fines-shop-webapp.io/avaibilitydetail" className={styles.container}>
+                <div>
+                    Наличие
+                </div>
+            </Link>
     )
 }
 
